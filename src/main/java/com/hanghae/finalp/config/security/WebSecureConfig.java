681201e49203ba73ca368659ea925f0d83c47baa
woong7361 <woong7361@gmail.com>
2022-04-30 18:21:39 +0900

    triangle

--- src/main/java/com/hanghae/finalp/config/security/WebSecureConfig.java
+++ src/main/java/com/hanghae/finalp/config/security/WebSecureConfig.java
@@ -2,7 +2,6 @@ package com.hanghae.finalp.config.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
